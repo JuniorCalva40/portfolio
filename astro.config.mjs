4af5b586bbaba345f7bb9c82df395615c3066fae
Junior Calva <juniorcalva40@gmail.com>
2024-01-07 23:30:01 -0500

    Fix Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://JuniorCalva40.github.io",
+  site: "https://juniorcalva40.github.io",
   base: "/portfolio",
 });
